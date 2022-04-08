d4b842af52aedcf9f64895ae7ce874d045e67f03
colinluzio <colinluzio@yahoo.co.uk>
2022-04-08 16:06:10 +0100

    remove button from layout

--- stories/StoryLayout.tsx
+++ stories/StoryLayout.tsx
@@ -13,7 +13,6 @@ interface IStoryArgs {
 const StoryLayout = (args: IStoryArgs) => {
     return (
         <div className={classNames({ "dark bg-gray-900": args.darkMode }, "-m-4")}>
-            <button className="btn-base btn-md btn-primary btn-primary-hover btn-primary-focus shadow-grayDark">Button CTA</button>
             <div className={classNames(args.className, { "p-4": !args.noPadding })}>
                 {args.children}
             </div>
