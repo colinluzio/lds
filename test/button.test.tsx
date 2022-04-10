9f1a381d8d7e7e539d6959b22e1c72f9d7207087
colinluzio <colinluzio@yahoo.co.uk>
2022-04-10 06:10:20 +0100

    remove redundant test

--- test/button.test.tsx
+++ test/button.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Default as Thing } from '../stories/Thing.stories';
+import { Button } from '../src/Button';
 
-describe('Thing', () => {
+describe('Button', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<Button variant='primary' />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
